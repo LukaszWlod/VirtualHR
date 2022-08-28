f0d7dcfa856d2e3606214d909d26cd595894304c
LukaszWlod <lukasz1023@gmail.com>
2022-08-28 10:32:06 +0200

    Initial commit

---
+++ src/main/java/com/githublukaszwlod/virtualhr/VirtualHrApplication.java
@@ -0,0 +1,13 @@
+package com.githublukaszwlod.virtualhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualHrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualHrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/githublukaszwlod/virtualhr/VirtualHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githublukaszwlod.virtualhr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualHrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
